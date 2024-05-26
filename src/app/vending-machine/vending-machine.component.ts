db00e9ed312e9acbab303b14f7fb65e0f925b2ac
SumoPod <visfulgencio@gmail.com>
2024-05-26 21:57:22 +0200

    Maslow 2 Test

--- src/app/vending-machine/vending-machine.component.ts
+++ src/app/vending-machine/vending-machine.component.ts
@@ -47,7 +47,7 @@ export class VendingMachineComponent implements OnInit{
     {
       let vendingContract = new this.web3.eth.Contract(VENDING_MACHINE_ABI, this.VendingMachine_Address);
 
-      let smartObjectId = "45228697695947564033082854924954193006092773360381611920298456273008413001782";
+      let smartObjectId = "70860015881416286250264768493881199026372532447349543657468792188196383309615";
       let commonOreTypeId = "54949089622078329307676094148632864879426651785510047822079265544250486580483";
       vendingContract.methods.test2__executeVendingMachine( smartObjectId,1,commonOreTypeId).send(
         {
@@ -62,5 +62,61 @@ export class VendingMachineComponent implements OnInit{
         console.log('Vending complete');
       });
     }
+  }
+
+  doConfig()
+  {
+    console.log('Config...');
+    
+    let vendingContract = new this.web3.eth.Contract(VENDING_MACHINE_ABI, this.VendingMachine_Address);
+
+    let smartObjectId = "70860015881416286250264768493881199026372532447349543657468792188196383309615";
+    // Common ore
+    let itemIn = "54949089622078329307676094148632864879426651785510047822079265544250486580483";
+    // Carbonaceous Ore
+    // let itemOut = "9540969374646031328134197690309428632894452754236413416084198707556493884019";
+    // Water Ice
+    let itemOut = "4849213586578758516979693453515271935121030808792700292547098743263487050031";
+    let quantityIn = 1;
+    let quantityOut = 1;
+
+    vendingContract.methods.test2__setVendingMachineRatio( smartObjectId, itemIn, itemOut, quantityIn, quantityOut).send(
+      {
+        from: this.walletAddress,
+      }
+    ).then((receipt: any) => {
+      console.log('Receipt:', receipt);
+      console.log('Hash:', receipt.transactionHash);
+    }).catch((error: any) => {
+      console.error('Error: ' + error.error.message);
+    }).finally(() => {
+      console.log('Config complete');
+    });
+
+    // {
+    //   "name": "smartObjectId",
+    //   "type": "uint256",
+    //   "internalType": "uint256"
+    // },
+    // {
+    //   "name": "inventoryItemIdIn",
+    //   "type": "uint256",
+    //   "internalType": "uint256"
+    // },
+    // {
+    //   "name": "inventoryItemIdOut",
+    //   "type": "uint256",
+    //   "internalType": "uint256"
+    // },
+    // {
+    //   "name": "quantityIn",
+    //   "type": "uint256",
+    //   "internalType": "uint256"
+    // },
+    // {
+    //   "name": "quantityOut",
+    //   "type": "uint256",
+    //   "internalType": "uint256"
+    // }
   }
 }
