42d2f7535dd957ef583d7575508e9983aa27d1c0
SumoPod <visfulgencio@gmail.com>
2024-06-02 21:35:38 +0200

    BRowser test

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
   brightQuantumColor = 'hsla(26, 85%, 58%, 1)';
+  text: string = "";
   constructor() {}
+
+  ngOnInit() {
+    console.log('Home Component Initialized');
+    window.addEventListener("eip6963:announceProvider", (event: any) => {
+      console.log("EIP6963 Provider Announced");
+      this.text = JSON.stringify( this.text )
+    });
+  }
 }
